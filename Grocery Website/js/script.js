31149078cb73776ee157cb89e8ddd12b78d8fb66
M Guru Prasad <76446944+IamGP-code@users.noreply.github.com>
2021-05-22 21:26:59 +0530

    Add files via upload

---
+++ Grocery Website/js/script.js
@@ -0,0 +1,52 @@
+// ---------------------------------------------Search Filter------------------------------------------//
+const btns = document.querySelectorAll('.btn-top');
+const storeProducts = document.querySelectorAll('.store-product');
+// const search = document.getElementById(search);
+
+for (i = 0; i < btns.length; i++) {
+
+    btns[i].addEventListener('click', (e) => {
+        e.preventDefault()
+        
+        const filter = e.target.dataset.filter;
+        console.log(filter);
+        
+        storeProducts.forEach((product)=> {
+            if (filter === 'all'){
+                product.style.display = 'inline-block'
+            } else {
+                if (product.classList.contains(filter)){
+                    product.style.display = 'inline-block'
+                } else {
+                    product.style.display = 'none'
+                }
+            }
+        });
+    });
+};
+
+// SEARCH FILTER
+const search = document.getElementById("search");
+const productName = document.querySelectorAll("div h2");
+
+// A BETTER WAY TO FILTER THROUGH THE PRODUCTS
+search.addEventListener("keyup", filterProducts);
+
+
+function filterProducts(e) {
+    const text = e.target.value.toLowerCase();
+    // console.log(productName[0]);
+    productName.forEach(function(product) {
+        const item = product.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) {
+            product.parentElement.parentElement.style.display = 'inline-block'
+        } else {
+            product.parentElement.parentElement.style.display = "none"
+        }
+    })
+}
+
+// -----------------------------------------Search filter End----------------------------------------------//
+
+
+
